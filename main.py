770c9d62e6a75673b05c5a88f6f7fe18301d3a36
SachinRagu <sachinragu2@gmail.com>
2023-09-02 23:28:16 +0100

    First commit

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-from bardapi import Bard
-import os
-
-
-
+import bardapi as Bard
 
 token = "aghvyYEtYc7tIEMHmBx5Tkyjeo7DNjg9V0SYqWZlmq_gtBiPrPHVKnbMx7pnCaDP98hUNA."
 bard = Bard(token=token)
-answer = bard.get_answer("who is jordinidian??")['content']
+answer = bard.get_answer("Green chutney vs red chutney?")['content']
 print(answer)
