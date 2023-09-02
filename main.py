1dd657ebeb52cf6ded6e23fd7e3092b37cf09a33
suvir <mrsuvir@gmail.com>
2023-09-02 23:52:52 +0100

    second commit

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ import bardapi as Bard
 
 token = "aghvyV3AkqdVIl_a4Qmwsf844o8-l_zCIh8qVXiuBsVoU_1KeajaTGqEmc2FOtTaLn_6-A."
 bard = Bard(token=token)
+#getting the answer from bard k;askf a dlaKJSDhlk hello world
 answer = bard.get_answer("Green chutney vs red chutney?")['content']
 print(answer)
