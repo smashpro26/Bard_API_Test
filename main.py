a5554e5bab08e64bc53a542d68c9bc6ada8e61f9
suvir <mrsuvir@gmail.com>
2023-09-03 20:06:47 +0100

    First commit to the dev branch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import bardapi as Bard
+#This comment was added from a the dev branch
+
 
 token = "aghvyV3AkqdVIl_a4Qmwsf844o8-l_zCIh8qVXiuBsVoU_1KeajaTGqEmc2FOtTaLn_6-A."
 bard = Bard(token=token)
