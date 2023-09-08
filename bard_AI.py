7dfbea779122779328506444ad0f49f9b02dc530
suvir <mrsuvir@gmail.com>
2023-09-08 21:28:07 +0100

    Co-authored-by: SachinRagu <SachinRagu@users.noreply.github.com>

--- bard_AI.py
+++ bard_AI.py
@@ -1,4 +1,7 @@
 from bardapi import Bard
+import os
+
+#token = os.environ.get('BARD_API_KEY')
 
 token = "aghvyV3AkqdVIl_a4Qmwsf844o8-l_zCIh8qVXiuBsVoU_1KeajaTGqEmc2FOtTaLn_6-A."
 bard = Bard(token=token)
