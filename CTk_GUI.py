63a35dc0f2eaf8581469dee874a948a3fce92647
suvir <mrsuvir@gmail.com>
2023-09-11 21:04:37 +0100

    changed the ui

--- CTk_GUI.py
+++ CTk_GUI.py
@@ -8,13 +8,14 @@ class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
         global entry
+        self.grid_columnconfigure(0, weight=1)
         self.geometry("1000x600")
         self.title("Veg biriyani ahh")
         entry = customtkinter.CTkEntry(self, placeholder_text="Enter your prompt")
-        entry.pack()
+        entry.grid(row=0, column= 0, padx=20,pady=20,sticky = "ew", columnspan=2)
 
         searchButton = customtkinter.CTkButton(self, text= "Ask Bard...",command= bardGetAnswer)
-        searchButton.pack()
+        searchButton.grid(row=1, column=0, padx=20, pady=10, sticky="w")
 
 def bardGetAnswer():
     prompt = entry.get()
